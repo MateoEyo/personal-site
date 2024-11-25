5b51e8f56b4c56f74d1a8165b6b281a59a90f108
MateoEyo <mateo.eyo@gmail.com>
2024-11-24 21:29:22 -0500

    updated padding

--- resume/src/app/landing.tsx
+++ resume/src/app/landing.tsx
@@ -6,11 +6,11 @@ import Github from "../components/buttons/github";
 export function Landing({ title }: { title: string }) {
   return (
     <header className="bg-sky-500/100 flex flex-col h-screen w-full items-center justify-center">
-      <h1 className="text-7xl p-4 font-bold text-center">
-        Hi, I'm Mateo
+      <h1 className="text-7xl p-4 pb-24 font-bold text-center">
+        Hi, I'm Mateo.
       </h1>
-      <p className="text-xl w-9/12 p-4 text-center">
-        Thank you for coming to my website! I'm excited to tell you a bit about myself.
+      <p className="text-xl w-9/12 p-4 pb-24 text-center">
+        Thank you for coming to my website. I'm using this page to learn more about web development.
       </p>
       <div className="grid grid-cols-2 gap-4 p-4">
         <Github />
