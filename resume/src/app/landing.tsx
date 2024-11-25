e085e943e828e5541a5b208e208c434e78509572
MateoEyo <mateo.eyo@gmail.com>
2024-11-25 06:35:57 -0500

    button now scrolls down to the about section

--- resume/src/app/landing.tsx
+++ resume/src/app/landing.tsx
@@ -2,8 +2,14 @@
 import LinkedIn from "../components/buttons/linkedin";
 import Github from "../components/buttons/github";
 
-
 export function Landing({ title }: { title: string }) {
+
+  function scrollToAbout() {
+    document.getElementById('aboutSection')?.scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
+
   return (
     <header className="bg-[#DFC5FE] flex flex-col h-screen w-full items-center justify-center">
       <h1 className="text-7xl p-4 pb-24 font-bold text-center">
@@ -16,7 +22,7 @@ export function Landing({ title }: { title: string }) {
         <Github />
         <LinkedIn />
       </div>
-      <button className="absolute bottom-0 py-4 hover:drop-shadow">
+      <button className="absolute bottom-0 py-4 hover:drop-shadow" onClick={scrollToAbout}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-12">
           <path stroke-linecap="round" stroke-linejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
         </svg>
