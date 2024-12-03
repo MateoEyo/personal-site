ec992eb36b3747afc4197616c761c708d4833262
MateoEyo <mateo.eyo@gmail.com>
2024-12-03 13:50:59 -0500

    skeleton for contact section

--- resume/src/app/contact.tsx
+++ resume/src/app/contact.tsx
@@ -4,9 +4,40 @@ export function Contact({ }: {  }) {
       <h1 className="resumeBasic-header">
         contact
       </h1>
-      <p className="resumeBasic-text">
-        Contact text
-      </p>
+      <div className="resumeBasic-text">
+        <form>
+          <div className="py-3">
+            <label className="font-medium">
+              Email
+            </label>
+            <div className="pt-2">
+              <input 
+                className="rounded-md pl-3 py-1 outline outline-1 -outline-offset-1 outline-gray-300" 
+                id="contact-subject-line" name="contact-subject-line" type="text" placeholder="email@example.com" />
+            </div>
+          </div>
+          <div className="py-3">
+            <label className="font-medium">
+              Subject Line
+            </label>
+            <div className="pt-2">
+              <input 
+                className="rounded-md pl-3 py-1 outline outline-1 -outline-offset-1 outline-gray-300" 
+                id="contact-subject-line" name="contact-subject-line" type="text" placeholder="The subject of this email" />
+            </div>
+          </div>
+          <div className="py-3">
+            <label className="font-medium">
+              Message
+            </label>
+            <div className="pt-2">
+              <input 
+                className="rounded-md pl-3 py-1 outline outline-1 -outline-offset-1 outline-gray-300 w-5/6" 
+                id="contact-subject-line" name="contact-subject-line" type="text" placeholder="The message of this email" />
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
     );
   }
