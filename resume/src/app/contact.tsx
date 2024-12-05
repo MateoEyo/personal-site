23ce7234cc50115b3f67933da3c560a340504697
MateoEyo <mateo.eyo@gmail.com>
2024-12-05 07:06:22 -0500

    removed subject line and added name

--- resume/src/app/contact.tsx
+++ resume/src/app/contact.tsx
@@ -4,7 +4,7 @@ export function Contact() {
     return (
       <div className="resumeBasic bg-white">
         <h1 className="resumeBasic-header">
-          <span className="bg-[#DFC5FE] px-2 pb-2"> contact </span>
+          <span className="bg-[#DFC5FE] px-2 pb-2"> have a question? </span>
         </h1>
         <div className="resumeBasic-text">
           <ContactMe />
