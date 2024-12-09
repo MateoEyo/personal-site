bc8c65101ea7d67afdaaf43b5253381075cf6d4b
MateoEyo <mateo.eyo@gmail.com>
2024-12-09 13:28:19 -0500

    base navbar

---
+++ resume/src/components/navbar/navbar.tsx
@@ -0,0 +1,20 @@
+export function Navbar() {
+    return (
+        <div className="navbar">
+            <ul>
+                <li className="navbar-items">
+                    Home
+                </li>
+                <li className="navbar-items">
+                    About
+                </li>
+                <li className="navbar-items">
+                    Experience
+                </li>
+                <li className="navbar-items">
+                    Contact
+                </li>
+            </ul>
+        </div>
+    )
+};
