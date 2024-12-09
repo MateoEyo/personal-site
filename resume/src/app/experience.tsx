febc4068b3290db968b01cbba4577f70b31c8996
MateoEyo <mateo.eyo@gmail.com>
2024-12-09 16:39:41 -0500

    I had console errors that these changes fixed

--- resume/src/app/experience.tsx
+++ resume/src/app/experience.tsx
@@ -5,7 +5,7 @@ export function Experience() {
         <span className="px-2 pb-2"> experience </span>
       </h1>
       <div className="resumeBasic-text">
-        <p className="resumeBasic-para">
+        <div className="resumeBasic-para">
           {/* Product Ownership part */}
           <p className="text-3xl pb-2">
             Education
@@ -14,9 +14,9 @@ export function Experience() {
             I have a Masters in Computer Science and certification of Cybersecurity from the University of Louisville and a Bachelors of Science in 
             Computer Science with a minor in Mathematics from the Univeristy of Kentucky.
           </p>
-        </p>
+        </div>
         {/* QA part */}
-        <p className="resumeBasic-para">
+        <div className="resumeBasic-para">
           <p className="text-3xl pb-2">
             Agile Methodologies
           </p>
@@ -24,9 +24,9 @@ export function Experience() {
             I am currently a Senior Product Owner with a Scrum Master certification from <a href="https://scrum.org">Scrum.org</a> and I have spent
             10+ years developing Agile practices on teams to best deliver efficient and practical incremental progress toward large multi-team projects. 
           </p>
-        </p>
+        </div>
         {/* Software Engineer part */}
-        <p className="resumeBasic-para">
+        <div className="resumeBasic-para">
           <p className="text-3xl pb-2">
             Software Engineering
           </p>
@@ -34,7 +34,7 @@ export function Experience() {
             I have used Python, Java, and C++ throughout my career to develop internal tools, write automation, and implement projects 
             in my college career. I am using this site as a way to teach myself more about Web Development with a focus in React development.
           </p>
-        </p>
+        </div>
       </div>
     </div>
     );
--- resume/src/app/landing.tsx
+++ resume/src/app/landing.tsx
@@ -19,8 +19,8 @@ export function Landing({ title }: { title: string }) {
       </div>
       <div className="btn-down-arrow" >
         <button onClick={() => scrollToSection('about')}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-12">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-12">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
           </svg>
         </button>
       </div>
