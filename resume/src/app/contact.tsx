ebc2848a32075ae8f5e4c7a2ae8a35ce211463b0
MateoEyo <mateo.eyo@gmail.com>
2024-12-09 20:26:12 -0500

    Update resume/src/app/contact.tsx
    Co-authored-by: Tyler Price <33133203+tylerprice1@users.noreply.github.com>

--- resume/src/app/contact.tsx
+++ resume/src/app/contact.tsx
@@ -2,7 +2,7 @@ import { ContactMe } from "../components/forms/contact-me";
 
 export function Contact() {
     return (
-      <div id = "contact" className="resumeBasic">
+      <div id="contact" className="resumeBasic">
         <h1 className="resumeBasic-header">
           <span className="px-2 pb-2"> have a question? </span>
         </h1>
