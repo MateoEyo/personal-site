1034e5b67702904d63ac5e2ce98f3e1f05df96b1
MateoEyo <mateo.eyo@gmail.com>
2024-12-12 16:04:13 -0500

    initial attempt at 'sliding from right side' menu when on mobile

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -10,54 +10,77 @@ export function Navbar(props: NavbarProps) {
   const { isDarkMode, onIsDarkModeChange } = props;
 
   return (
-    <div className="navbar">
-      <div></div>
-
-      <ul>
-        <div className="nav-item-container">
-          <button>
-            <li className="nav-item" onClick={() => scrollToSection('home')}>
-              Home
-            </li>
-          </button>
+    <div >
+      <div className='nav-btn'>
+        <button className='btn-contact' onClick={openNav}>menu</button> 
+      </div>
+      <div id='navbar' className="navbar">
+        <div>
+          logo
         </div>
 
-        <div className="nav-item-container">
-          <button>
-            <li className="nav-item" onClick={() => scrollToSection('about')}>
-              About
-            </li>
-          </button>
-        </div>
+        <ul>
+          <div className="nav-item-container">
+            <button>
+              <li className="nav-item" onClick={() => scrollToSection('home')}>
+                Home
+              </li>
+            </button>
+          </div>
 
-        <div className="nav-item-container">
-          <button>
-            <li
-              className="nav-item"
-              onClick={() => scrollToSection('experience')}
-            >
-              Experience
-            </li>
-          </button>
-        </div>
+          <div className="nav-item-container">
+            <button>
+              <li className="nav-item" onClick={() => scrollToSection('about')}>
+                About
+              </li>
+            </button>
+          </div>
 
-        <div className="nav-item-container">
-          <button>
-            <li className="nav-item" onClick={() => scrollToSection('contact')}>
-              Contact
-            </li>
-          </button>
-        </div>
-      </ul>
+          <div className="nav-item-container">
+            <button>
+              <li
+                className="nav-item"
+                onClick={() => scrollToSection('experience')}
+              >
+                Experience
+              </li>
+            </button>
+          </div>
 
-      <div>
-        <Toggle
-          isOn={isDarkMode}
-          onChange={(isOn) => {
-            onIsDarkModeChange(isOn);
-          }}
-        />
+          <div className="nav-item-container">
+            <button>
+              <li className="nav-item" onClick={() => scrollToSection('contact')}>
+                Contact
+              </li>
+            </button>
+          </div>
+        </ul>
+
+        <div>
+          <Toggle
+            isOn={isDarkMode}
+            onChange={(isOn) => {
+              onIsDarkModeChange(isOn);
+            }}
+          />
+        </div>
       </div>
     </div>
   );
+}
+
+function openNav() {
+  const navbarElement = document.getElementById('navbar');
+  if (navbarElement !== null) {
+    navbarElement.style.width = "100%";
+    navbarElement.style.display = "inline";
+  }
+}
+
+function closeNav() {
+  const navbarElement = document.getElementById('navbar');
+  if (navbarElement !== null) {
+    navbarElement.style.width = "0";
+    navbarElement.style.display = "none";
+  }
 }
