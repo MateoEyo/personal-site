fb1e642110739a5a3755dc1d14387ec2b70564e1
MateoEyo <mateo.eyo@gmail.com>
2024-12-16 09:11:51 -0500

    I'm not sure why main was broken but I think I fixed it

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,15 +4,12 @@ import About from './about';
 import Contact from './contact';
 import Experience from './experience';
 import Landing from './landing';
+import { useIsMobile } from '../components/functions/screen-size';
 
 export function App() {
   const [isDarkMode, setIsDarkMode] = useState<boolean>(
     () => localStorage.getItem('theme') === 'dark'
   );
-
-  const isMobile = useIsMobile();
-    () => window.screen.width >= 480
-  )
 
   return (
     <div className={`app theme-${isDarkMode ? 'dark' : 'light'}`}>
